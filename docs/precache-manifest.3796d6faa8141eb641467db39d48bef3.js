5fc722ffc53c9e98f5b83aa1a485285bd5194382
Francisco Zambrano <franciscozambrano@outlook.es>
2020-08-02 22:26:06 -0500

    Docs folder added

---
+++ docs/precache-manifest.3796d6faa8141eb641467db39d48bef3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fb1a8ebe22751023a07c78d6a6cfcf3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb9c8081d6a67e4629cc",
+    "url": "/static/css/main.a15d35bf.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb9c8081d6a67e4629cc",
+    "url": "/static/js/main.0619f717.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
